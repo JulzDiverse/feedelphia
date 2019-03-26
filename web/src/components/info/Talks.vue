cf6010c0e362676cee2dae7014af7aebf3130b1d
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2019-03-26 15:59:58 +0100

    add info components
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

---
+++ web/src/components/info/Talks.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container justify-center>
+    <v-layout align-center justify-center>
+      <v-flex xs-6>
+        <div class="title text-xs-center text-md-center">Speakers</div>
+          <div class="subheading text-xs-center text-md-center" ><a href="https://twitter.com/doctor_julz">Julz Friedman (PM)</a></div>
+          <div class="subheading text-xs-center text-md-center" ><a href="https://twitter.com/suhlig">Steffen Uhlig (Anchor)</a></div>
+          <div class="subheading text-xs-center text-md-center" ><a href="https://twitter.com/herrjulz">Julz Skupnjak </a></div>
+          <div class="subheading text-xs-center text-md-center" ><a href="https://twitter.com/mnitchev">Mario Nitchev </a></div>
+          <div class="subheading text-xs-center text-md-center" ><a href="https://twitter.com/gdankov">Georgi Dankov </a></div>
+          <div class="subheading text-xs-center text-md-center" ><a href="https://twitter.com/alex_sly">Oleksandr Slynko</a></div>
+          <div class="subheading text-xs-center text-md-center" ><a href="https://twitter.com/AkshayMankar">Akshay Mankar</a></div>
+      </v-flex>
+     </v-layout>
+    <v-layout align-center justify-center>
+      <v-img
+         position="center center"
+         :max-width=800
+         :src="require('@/assets/articles/eiriniers-summit.png')"
+      >
+      </v-img>
+     </v-layout>
+  </v-container>
+</template>
---
+++ web/src/views/Info.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="info">
+    <talks />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Info',
+
+    components: {
+      Talks: () => import('@/components/info/Talks')
+    }
+  }
+</script>
